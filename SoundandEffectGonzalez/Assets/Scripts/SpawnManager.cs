7a5403763de4e30974ec331a2b621bd7ed4916d2
Joel Gonzalez <754537@student.dusd.net>
2024-04-18 13:47:19 -0700

    todo bugeado como siempre

--- SoundandEffectGonzalez/Assets/Scripts/SpawnManager.cs
+++ SoundandEffectGonzalez/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3 (25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerControler playerControllerScript;
+    private int randomObstacle;
 
 
     // Start is called before the first frame update
@@ -26,8 +27,12 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-       
-            Instantiate(obstaclePrefab,spawnPos, obstaclePrefab.transform.rotation);
+       if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+        }
+           
         
     }
 }
